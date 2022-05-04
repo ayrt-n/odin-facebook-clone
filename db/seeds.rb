e4d2b11146dd96bf864fbf30dbaf1cf186ff370a
ayrton <ayrton.parkinson1@gmail.com>
2022-05-04 08:14:05 -0400

    Add seed user data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create(email: 'tester1@gmail.com', password: 'password')
+User.create(email: 'tester2@gmail.com', password: 'password')

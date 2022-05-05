0952b338b78fc912b6c05bc338709fac5045cf8f
ayrton <ayrton.parkinson1@gmail.com>
2022-05-05 06:30:37 -0400

    Add basic #index and #create

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,17 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
+    @post = Post.new
+  end
+
+  def create
+    @post = current_user.posts.create(post_params)
+    @post.save
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:body)
   end
 end

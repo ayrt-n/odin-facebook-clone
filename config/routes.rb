00ca1a2792ba07ef740797c2d277935957c5d165
ayrton <ayrton.parkinson1@gmail.com>
2022-05-05 08:07:50 -0400

    Nest comment routes under posts to associate comment with posts

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'posts#index'
-  resources :posts, only: [:index, :new, :create]
+  resources :posts, only: [:index, :new, :create] do
+    resources :comments, only: [:create]
+  end
 end

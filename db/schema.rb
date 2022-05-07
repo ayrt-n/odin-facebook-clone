0e680a5ba29dc02ce199c055e8b76b7e5cc1a75b
ayrton <ayrton.parkinson1@gmail.com>
2022-05-07 10:07:23 -0400

    Create and migrate FriendRequests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_07_134646) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_07_140047) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,15 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_07_134646) do
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "friend_requests", force: :cascade do |t|
+    t.bigint "requester_id"
+    t.bigint "requestee_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["requestee_id"], name: "index_friend_requests_on_requestee_id"
+    t.index ["requester_id"], name: "index_friend_requests_on_requester_id"
   end
 
   create_table "friendships", force: :cascade do |t|
@@ -64,5 +73,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_07_134646) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "friend_requests", "users", column: "requestee_id"
+  add_foreign_key "friend_requests", "users", column: "requester_id"
   add_foreign_key "friendships", "users", column: "friend_id"
 end

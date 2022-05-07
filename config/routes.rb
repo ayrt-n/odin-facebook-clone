c84de5a8721802bec7742702a93de1a67cccd54c
ayrton <ayrton.parkinson1@gmail.com>
2022-05-07 10:56:10 -0400

    Add routes for friendrequests#create and #destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
+  root 'posts#index'
+
+  # Routes for users
   devise_for :users
   resources :users
-  root 'posts#index'
+  
+  # Routes for post-system
   resources :posts, only: [:index, :new, :create] do
     resources :comments, only: [:create]
     resource :like, only: [:create, :destroy]
   end
+
+  # Routes for friendship-system
   resources :friendships, only: [:create, :destroy]
+  resources :friend_requests, only: [:create, :destroy]
 end

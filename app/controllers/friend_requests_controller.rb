626d675581a301ac9ef6a04f71a09cf2acfb6c9d
ayrton <ayrton.parkinson1@gmail.com>
2022-05-07 12:03:13 -0400

    Add #index to display incoming friend requests

--- app/controllers/friend_requests_controller.rb
+++ app/controllers/friend_requests_controller.rb
@@ -1,10 +1,14 @@
 class FriendRequestsController < ApplicationController
+  def index
+    @friend_requests = current_user.incoming_friend_requests
+  end
+  
   def create
     friend_request = current_user.outgoing_friend_requests.build(requestee_id: params[:requestee_id])
     friend_request.save
   end
 
   def destroy
-    
+
   end
 end

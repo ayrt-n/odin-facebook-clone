b8a600cfefe22b6476c80e3f7a1563d6f419ecdf
ayrton <ayrton.parkinson1@gmail.com>
2022-05-07 10:38:26 -0400

    Add Friendship and FriendRequest associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  # Friendship-system associations
+  has_many :outgoing_friend_requests, class_name: 'FriendRequest', foreign_key: :requester_id
+  has_many :incoming_friend_requests, class_name: 'FriendRequest', foreign_key: :requestee_id
+  has_many :friendships, dependent: :destroy
+  has_many :friends, through: :friendships
+
+  # Post-system associations
   has_many :posts, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy

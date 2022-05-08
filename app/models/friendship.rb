b944fff084fdcef80b70458e252bccf30835a435
ayrton <ayrton.parkinson1@gmail.com>
2022-05-08 10:34:59 -0400

    Add #create_recipricol_friendship callback to after_create

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -3,4 +3,14 @@ class Friendship < ApplicationRecord
   belongs_to :friend, class_name: 'User'
 
   validates :user_id, uniqueness: { scope: :friend_id }
+
+  after_create :create_recipricol_friendship
+
+  private
+
+  def create_recipricol_friendship
+    return if Friendship.where(user_id: friend_id, friend_id: user_id).exists?
+
+    Friendship.create(user_id: friend_id, friend_id: user_id)
+  end
 end

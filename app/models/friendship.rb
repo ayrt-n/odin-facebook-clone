7d37e99b5abfaa15af915dca0739e7f8ae623ee7
ayrton <ayrton.parkinson1@gmail.com>
2022-05-08 13:10:36 -0400

    Add validation to prevent user creating friendship with themselves

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -2,12 +2,17 @@ class Friendship < ApplicationRecord
   belongs_to :user
   belongs_to :friend, class_name: 'User'
 
-  validates :user_id, uniqueness: { scope: :friend_id }
+  validates :user_id, uniqueness: { scope: :friend_id, message: "friendship already exists" }
+  validate :check_self_friendship
 
   after_create :create_recipricol_friendship
   after_destroy :destroy_recipricol_friendship
 
   private
+
+  def check_self_friendship
+    errors.add(:user_id, "can't be friend with self") if user == friend
+  end
 
   def create_recipricol_friendship
     return if Friendship.where(user_id: friend_id, friend_id: user_id).exists?

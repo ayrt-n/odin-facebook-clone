bdcdf49a1d4ca10f3ae9b365d4dd04294df62d59
ayrton <ayrton.parkinson1@gmail.com>
2022-05-12 19:02:16 -0400

    Add test for invalid post inputs

--- spec/system/create_post_spec.rb
+++ spec/system/create_post_spec.rb
@@ -6,10 +6,17 @@ RSpec.describe 'Creating a post', type: :system do
     login_as(user, :scope => :user)
   end
 
-  scenario 'with valid inputs' do
+  scenario 'with valid input' do
     visit posts_path
     fill_in 'New post', with: 'Test post!'
     click_on 'Create Post'
     expect(page).to have_content('Test post!')
+  end
+
+  scenario 'with blank input' do
+    visit posts_path
+    fill_in 'New post', with: ''
+    click_on 'Create Post'
+    expect(page).to have_content("New post can't be blank")
   end
 end

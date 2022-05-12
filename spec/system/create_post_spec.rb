a39f6f2f54396382b5c2186b0dff3ad2fe34e8aa
ayrton <ayrton.parkinson1@gmail.com>
2022-05-12 08:08:53 -0400

    Add test for creating new post

---
+++ spec/system/create_post_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe 'Creating a post', type: :system do
+  before do
+    user = FactoryBot.create(:user)
+    login_as(user, :scope => :user)
+  end
+
+  scenario 'with valid inputs' do
+    visit posts_path
+    fill_in 'New post', with: 'Test post!'
+    click_on 'Create Post'
+    sleep(5)
+    expect(page).to have_content('Test post!')
+  end
+end

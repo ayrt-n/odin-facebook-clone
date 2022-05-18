cec4fb05b175e8dbbc445dce9cbf75a348477ab2
ayrton <ayrton.parkinson1@gmail.com>
2022-05-18 08:19:25 -0400

    Add #destroy and #verify_comment_author

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,28 @@
 class CommentsController < ApplicationController
+  before_action :verify_comment_author, only: [:destroy]
+
   def create
     comment = current_user.comments.build(comment_params)
     comment.save
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
   end
 
   private
 
   def comment_params
     params.require(:comment).permit(:body).merge(post_id: params[:post_id])
+  end
+
+  def verify_comment_author
+    @comment = Comment.find(params[:id])
+
+    unless current_user == @comment.user
+      flash[:error] = "You do not have the correct permssions to do this"
+      redirect_to posts_path
+    end
   end
 end

131b4dd2ab4e4203f5b2f35a4ad74c65a0237171
ayrton <ayrton.parkinson1@gmail.com>
2022-05-28 13:08:45 -0400

    Add #show method to view user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,10 @@ class UsersController < ApplicationController
   def index
     @users = User.not_friends_with(current_user)
   end
+
+  def show
+    @user = User.find(params[:id])
+    @new_comment = Comment.new
+  end
+
 end

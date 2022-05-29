4f5b7b5b1a4b8fe2c1237e6d64931ec96b133b8a
ayrton <ayrton.parkinson1@gmail.com>
2022-05-29 12:44:40 -0400

    Add profile picture associations and #add_default_avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,6 +20,11 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
+  # Avatar associations
+  has_one_attached :avatar
+
+  after_commit :add_default_avatar, on: [:create, :update]
+
   scope :not_friends_with, -> (user) { where.not(id: user.friends.ids.push(user.id)) }
 
   def self.from_omniauth(auth)
@@ -29,6 +34,21 @@ class User < ApplicationRecord
       user.username = auth.info.nickname
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
+    end
+  end
+
+  private
+
+  def add_default_avatar
+    unless avatar.attached?
+      avatar.attach(
+        io: File.open(
+          Rails.root.join(
+            'app', 'assets', 'images', 'default_avatar.png'
+          )
+        ), filename: 'default_avatar.png',
+        content_type: 'image/png'
+      )
     end
   end
 end

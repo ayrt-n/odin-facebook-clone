2efa647e5adbd33f3a5417fcdcc755f0eb9510d1
ayrton <ayrton.parkinson1@gmail.com>
2022-05-29 20:00:44 -0400

    Add username to users factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,7 @@
 # User Factory for creating/building user in testing/development
 FactoryBot.define do
   factory :user do
+    username { Faker::Name.name }
     email { Faker::Internet.email }
     password { 'password' }
 

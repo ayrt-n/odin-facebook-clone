bcb4e5fdcff53646fbbc15432eae51f1ee7b26fb
ayrton <ayrton.parkinson1@gmail.com>
2022-06-02 06:47:30 -0400

    Add additional queries and #includes to fix N+1 problems

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,16 @@
 class UsersController < ApplicationController
   def index
+    # Load all users not currently friends with the current user
     @users = User.not_friends_with(current_user)
+
+    # Load pending friend requests to determine available action in view
+    @current_user_pending_friend_ids = current_user.pending_friend_ids
   end
 
   def show
     @user = User.find(params[:id])
-    @posts = @user.posts.order("created_at DESC")
+    @posts = @user.posts.includes(:comments, :likes).order("created_at DESC")
+    @current_user_likes = @user.posts.includes(:likes).where(likes: { user_id: current_user })
     @new_comment = Comment.new
   end
 

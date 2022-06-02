fb82d0fb0ff56a0b5dadad81e4322663bb53e509
ayrton <ayrton.parkinson1@gmail.com>
2022-06-02 08:01:12 -0400

    Add helper methods #timeline_by_user and #liked_by to perform common queries easier

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,11 +5,17 @@ class Post < ApplicationRecord
 
   validates :body, presence: true
 
-  scope :posted_by, -> (user_ids) { where user_id: user_ids }
+  scope :posted_by, -> (users) { where user: users }
+  scope :timeline_by_users, -> (users) { includes({ comments: :user }, { likes: :user }, :user).posted_by(users).order('created_at DESC') }
 
   def liked?
     return false if self.likes_count.nil? || self.likes_count.zero?
 
     true
+  end
+
+  def liked_by?(user)
+    liked_by = likes.collect(&:user_id)
+    liked_by.include?(user.id)
   end
 end

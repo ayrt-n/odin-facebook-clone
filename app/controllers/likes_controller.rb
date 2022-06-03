d518651955f809e974dcdfc20028a6f813d733dc
ayrton <ayrton.parkinson1@gmail.com>
2022-06-03 08:23:05 -0400

    Add turbostream to post likes

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,11 +1,17 @@
 class LikesController < ApplicationController
   def create
-    like = Like.new(post_id: params[:post_id], user_id: current_user.id)
-    like.save
+    @like = Like.new(post_id: params[:post_id], user_id: current_user.id)
+    @post = Post.find(params[:post_id])
+    @like.save
+
+    redirect_to post_path(@post)
   end
 
   def destroy
-    like = Like.where("post_id = ? AND user_id = ?", params[:post_id], current_user.id)
-    like.destroy_all
+    @like = Like.where("post_id = ? AND user_id = ?", params[:post_id], current_user.id)
+    @post = Post.find(params[:post_id])
+    @like.destroy_all
+
+    redirect_to post_path(@post), status: 303
   end
 end

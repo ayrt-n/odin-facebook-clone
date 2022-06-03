9760f0eae32fdfae5ee834989f473590cd689266
ayrton <ayrton.parkinson1@gmail.com>
2022-06-03 07:30:45 -0400

    Add turbostream to post comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,8 +2,21 @@ class CommentsController < ApplicationController
   before_action :verify_comment_author, only: [:destroy]
 
   def create
-    comment = current_user.comments.build(comment_params)
-    comment.save
+    @comment = current_user.comments.build(comment_params)
+    @post = Post.find(params[:post_id])
+
+    respond_to do |format|
+      if @comment.save
+        format.turbo_stream { 
+            render turbo_stream: turbo_stream.append("post_#{params[:post_id]}_comments",
+                                                     partial: 'comment',
+                                                     locals: { comment: @comment, post: @post })
+          }
+        format.html { redirect_to @post }
+      else
+        format.html{ render :new, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy

abcf9214b11484e32fdc668d0f5f1d7d5f8f80c6
ayrton <ayrton.parkinson1@gmail.com>
2022-06-04 13:17:30 -0400

    Add devise users registrations controller route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'posts#index'
 
   # Routes for users
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'users/registrations' }
   resources :users
 
   # Routes for post-system

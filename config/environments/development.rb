e6eab0d271045532371b43cf07951c3ee6a260d1
ayrton <ayrton.parkinson1@gmail.com>
2022-06-04 10:39:06 -0400

    Set up dev config to send email with letter opener

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,6 +33,10 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+  # User letter_opener gem for emails
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
+
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 

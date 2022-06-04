1eec6e9b1e0536d62c50e4349eb0fc7c66570cb4
ayrton <ayrton.parkinson1@gmail.com>
2022-06-04 13:17:57 -0400

    Add after sign in and after sign out routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,12 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :avatar])
     devise_parameter_sanitizer.permit(:account_update, keys: [:avatar])
   end
+
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) || posts_path
+  end
+
+  def after_sign_out_path_for(resource)
+    new_user_session_path
+  end
 end

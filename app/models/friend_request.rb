b76f2ff89d572998c0ee59e3dd618a7eaf543421
ayrton <ayrton.parkinson1@gmail.com>
2022-06-11 09:53:53 -0400

    Add counter cache to incoming_frs to easily notify user of FRs

--- app/models/friend_request.rb
+++ app/models/friend_request.rb
@@ -1,6 +1,6 @@
 class FriendRequest < ApplicationRecord
   belongs_to :requester, class_name: 'User'
-  belongs_to :requestee, class_name: 'User'
+  belongs_to :requestee, class_name: 'User', counter_cache: :incoming_friend_requests_count
 
   validates :requester_id, uniqueness: { scope: :requestee_id }
 

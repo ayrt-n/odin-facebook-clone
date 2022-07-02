d1e2611d60a8a1a93ce9a0685e46c05f821aeba5
ayrton <ayrton.parkinson1@gmail.com>
2022-07-02 11:05:35 -0400

    Remove Friendships and add new FriendRequest methods to function as friendships

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,12 +9,13 @@ class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true
 
   # Friendship-system associations
-  has_many :outgoing_friend_requests, -> { where accepted: false }, class_name: 'FriendRequest', foreign_key: :requester_id, dependent: :destroy
-  has_many :incoming_friend_requests, -> { where accepted: false }, class_name: 'FriendRequest', foreign_key: :requestee_id, dependent: :destroy
+  has_many :outgoing_friend_requests, -> { where accepted: false }, class_name: 'FriendRequest',
+                                                                    foreign_key: :requester_id,
+                                                                    dependent: :destroy
+  has_many :incoming_friend_requests, -> { where accepted: false }, class_name: 'FriendRequest',
+                                                                    foreign_key: :requestee_id,
+                                                                    dependent: :destroy
   has_many :pending_friends, through: :outgoing_friend_requests, source: :requestee
-
-  has_many :friendships, dependent: :destroy
-  has_many :friends, through: :friendships
 
   # Post-system associations
   has_many :posts, dependent: :destroy
@@ -23,11 +24,13 @@ class User < ApplicationRecord
 
   # Avatar associations
   has_one_attached :avatar, dependent: :destroy
+  after_commit :add_default_avatar, on: %i[create update]
 
-  after_commit :add_default_avatar, on: [:create, :update]
+  # Callback for sending welcome email
   after_create :send_welcome_email
 
-  scope :not_friends_with, -> (user) { where.not(id: user.friends_list) }
+  # Scope to query for users which are not friends of a given user
+  scope :not_friends_with, -> (user) { where.not(id: user.friends_ids + [user.id]) }
 
   # Find or create new user using Omniauth
   def self.from_omniauth(auth)
@@ -38,33 +41,27 @@ class User < ApplicationRecord
     end
   end
 
-  # Return list of friends ids and current user id as array
-  def friends_list
-    friend_array = friendz.map do |friend|
-      if friend.requester_id == id
-        friend.requestee_id
-      else
-        friend.requester_id
-      end 
-    end
-
-    friend_array << id
+  # Return array of user friends ids
+  def friends_ids
+    friends.map(&:id)
   end
 
-  def friendz
-    FriendRequest.accepted_friend_requests(self)
+  # Return array of user friends (users)
+  def friends
+    accepted_frs = FriendRequest.accepted_friend_requests(self).with_user
+    accepted_frs.map { |fr| fr.requester_id == self.id ? fr.requestee : fr.requester }
   end
 
   # Check if user has a pending friend request with another user, returns true or false
   def pending_friend_request?(user)
-    friend_requests = self.incoming_friend_requests.collect(&:requester_id)
-    friend_requests = friend_requests.concat(self.outgoing_friend_requests.collect(&:requestee_id))
+    friend_requests = incoming_friend_requests.collect(&:requester_id)
+    friend_requests = friend_requests.concat(outgoing_friend_requests.collect(&:requestee_id))
     friend_requests.include?(user.id)
   end
 
   # Check if user has any incoming friend requests, returns true or false
   def incoming_friend_requests?
-    return false if self.incoming_friend_requests_count.nil? || self.incoming_friend_requests_count.zero?
+    return false if incoming_friend_requests_count.nil? || incoming_friend_requests_count.zero?
 
     true
   end

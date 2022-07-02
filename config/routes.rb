b6ac3bbce227fc0abca3ca796f5b6ae81b3f0e96
ayrton <ayrton.parkinson1@gmail.com>
2022-07-02 12:28:03 -0400

    Remove friendships routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,13 +7,12 @@ Rails.application.routes.draw do
 
   # Routes for post-system
   resources :posts do
-    resources :comments, only: [:create, :destroy]
-    resource :like, only: [:create, :destroy]
+    resources :comments, only: %i[create destroy]
+    resource :like, only: %i[create destroy]
   end
 
   # Routes for friendship-system
-  resources :friendships, only: [:index, :create, :destroy]
-  resources :friend_requests, only: [:index, :create, :destroy] do
+  resources :friend_requests, only: %i[index create destroy] do
     post :accept, on: :member
     post :decline, on: :member
   end

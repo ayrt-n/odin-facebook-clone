dd9ca6ae0ce89b6587efaf997f478819b67da416
ayrton <ayrton.parkinson1@gmail.com>
2022-07-02 11:05:03 -0400

    Update friend request class to function as friendships

--- app/models/friend_request.rb
+++ app/models/friend_request.rb
@@ -1,21 +1,27 @@
 class FriendRequest < ApplicationRecord
   belongs_to :requester, class_name: 'User'
-  belongs_to :requestee, class_name: 'User', counter_cache: :incoming_friend_requests_count
+  belongs_to :requestee, class_name: 'User'
 
-  validates :requester_id, uniqueness: { scope: :requestee_id }
+  validates :requester_id, uniqueness: { scope: :requestee_id, message: 'friend request already exists' }
 
   # Callbacks to update conditional counter
-  after_update :update_incoming_friend_requests_count
+  after_save :update_incoming_friend_requests_count
   after_destroy :update_incoming_friend_requests_count
 
+  # Scope to pull all accepted friend requests for a certain user (i.e., their friends)
   scope :accepted_friend_requests, -> (user) {
-    where('requester_id = ? OR requestee_id = ? AND accepted = true',
+    where('accepted = true AND (requester_id = ? OR requestee_id = ?)',
           user.id, user.id)
   }
+
+  # Scope to include the requester and requestee user
+  scope :with_user, -> { includes(:requester, :requestee) }
 
   private
 
   def update_incoming_friend_requests_count
-    self.requestee.incoming_friend_requests_count = FriendRequest.count(conditions: ['accepted = false AND requestee_id = ?', self.requestee.id])
+    requestee.update_attribute(:incoming_friend_requests_count,
+                               FriendRequest.where(accepted: false)
+                               .where(requestee: self.requestee).count)
   end
 end

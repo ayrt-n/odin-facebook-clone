08419c27e9d2184829bbc9b72c32c53887033302
ayrton <ayrton.parkinson1@gmail.com>
2022-07-04 07:53:28 -0400

    Remove uniqueness constraint on usernames

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 
   devise :omniauthable, omniauth_providers: %i[github]
 
-  validates :username, presence: true, uniqueness: true
+  validates :username, presence: true
 
   # Friendship-system associations
   has_many :outgoing_friend_requests, -> { where accepted: false }, class_name: 'FriendRequest',

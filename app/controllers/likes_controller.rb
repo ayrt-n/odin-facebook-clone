018e7e38d2c25d65df004a6d511ea102a6ad4fa9
ayrton <ayrton.parkinson1@gmail.com>
2022-07-05 08:04:02 -0400

    Add turbo stream format option to #create and #destroy

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,16 +2,40 @@ class LikesController < ApplicationController
   def create
     @like = Like.new(post_id: params[:post_id], user_id: current_user.id)
     @post = Post.find(params[:post_id])
-    @like.save
 
-    redirect_to post_path(@post)
+    respond_to do |format|
+      if @like.save
+        @post.reload
+
+        format.turbo_stream {
+          render turbo_stream: turbo_stream.update("post_#{params[:post_id]}_likes",
+                                                    partial: 'posts/post_likes',
+                                                    locals: { post: @post })
+        }
+        format.html { redirect_to @post }
+      else
+        redirect_to @post
+      end
+    end
   end
 
   def destroy
-    @like = Like.where("post_id = ? AND user_id = ?", params[:post_id], current_user.id)
+    @like = Like.where('post_id = ? AND user_id = ?', params[:post_id], current_user.id)
     @post = Post.find(params[:post_id])
-    @like.destroy_all
 
-    redirect_to post_path(@post), status: 303
+    respond_to do |format|
+      if @like.destroy_all
+        @post.reload
+
+        format.turbo_stream {
+          render turbo_stream: turbo_stream.update("post_#{params[:post_id]}_likes",
+                                                    partial: 'posts/post_likes',
+                                                    locals: { post: @post })
+        }
+        format.html { redirect_to @post, status: 303 }
+      else
+        redirect_to @post, status: 303
+      end
+    end
   end
 end

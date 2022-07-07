276e369818c9a1e941973eb24ede3dd08db4f074
ayrton <ayrton.parkinson1@gmail.com>
2022-07-07 07:12:09 -0400

    Update and style user welcome email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,6 +4,11 @@ class UserMailer < ApplicationMailer
   def welcome_email
     @user = params[:user]
     @login_url = new_user_session_url
+
+    attachments.inline['email_header.png'] = File.read("#{Rails.root}/app/assets/images/email_header.png")
+    attachments.inline['left_buddy.png'] = File.read("#{Rails.root}/app/assets/images/left_buddy.png")
+    attachments.inline['right_buddy.png'] = File.read("#{Rails.root}/app/assets/images/right_buddy.png")
+
     mail(to: @user.email, subject: 'Welcome to FaceBuddies!')
   end
 end

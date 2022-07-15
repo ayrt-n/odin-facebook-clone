08fe4277c6840123788ca0e69bda8411493fba0e
ayrton <ayrton.parkinson1@gmail.com>
2022-07-15 12:57:20 -0400

    Add flash to github signin failure

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -9,6 +9,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       set_flash_message(:notice, :success, kind: 'Github') if is_navigational_format?
     else
       session['devise.github_data'] = request.env['omniauth.auth'].except(:extra)
+      set_flash_message(:alert, :failure, kind: 'Github') if is_navigational_format?
       redirect_to new_user_registration_url
     end
   end

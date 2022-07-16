6675ea5c7e489d8cf1ee2b8297e342ca8f2d8b2a
ayrton <ayrton.parkinson1@gmail.com>
2022-07-16 11:42:42 -0400

    Add target to reset upload form field

--- app/javascript/controllers/reset_form_controller.js
+++ app/javascript/controllers/reset_form_controller.js
@@ -1,7 +1,13 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
+  static targets = ["upload"]
+
   reset() {
-   this.element.reset()
+    this.element.reset()
+    
+    if (this.hasUploadTarget) {
+      this.uploadTarget.innerHTML = "No file selected."
+    }
   }
 }

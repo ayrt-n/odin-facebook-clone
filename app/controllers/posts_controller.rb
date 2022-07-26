1b8d9193f32e714cf247eb9063e2abb62cabf9b7
ayrton <ayrton.parkinson1@gmail.com>
2022-07-26 07:48:46 -0400

    Fix N+1 problem in posts#show

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,7 +7,7 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
+    @post = Post.with_likes.with_comments.find(params[:id])
   end
 
   def new

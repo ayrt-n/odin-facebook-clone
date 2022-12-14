5126fd828783052b548f73b064fe2a52c1186009
ayrton <ayrton.parkinson1@gmail.com>
2022-12-14 16:04:52 -0500

    Add config.hosts to fix blocked host in prod

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -84,4 +84,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # Fix Blocked host exception
+  config.hosts << "odin-facebook-clone-production.up.railway.app"
 end
